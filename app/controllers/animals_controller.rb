4b0b8536e19b68771b6b5c1d2f831df1222e9bf6
Joao Marcos <jao.markos@hotmail.com>
2020-09-21 21:15:21 -0300

    Add edit, create and update methods to animals_controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,9 +1,39 @@
 class AnimalsController < ApplicationController
+  before_action :set_animal, only: %i[show edit update]
+
   def index
-    @animals = Animal.all
+    @animals = Animal.where(adopted: false).order(created_at: :desc)
   end
 
   def new
     @animal = Animal.new
+  end
+
+  def edit; end
+
+  def create
+    @animal = Animal.new(animal_params)
+
+    if @animal.save
+      redirect_to animals_path, notice: 'Pet was successfully added'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @animal.update(animal_params)
+      redirect_to animals_path, notice: 'Pet was successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  def set_animal
+    @animal = Animal.find(params[:id])
+  end
+
+  def animal_params
+    params.require(:animal).permit(:name, :date_of_birth, :adopted, :animal_type_id)
   end
 end
