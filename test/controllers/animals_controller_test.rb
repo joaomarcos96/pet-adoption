a50271ce63041ff196054252cd3a43dc6a727131
Joao Marcos <jao.markos@hotmail.com>
2020-09-23 20:10:50 -0300

    Add test for new method in animals_controller

--- test/controllers/animals_controller_test.rb
+++ test/controllers/animals_controller_test.rb
@@ -1,8 +1,27 @@
 require 'test_helper'
 
 class AnimalsControllerTest < ActionDispatch::IntegrationTest
+  # setup do
+  #   @animal = animals :dog_pet
+  # end
+
   test 'should get index' do
     get animals_url
     assert_response :success
   end
+
+  test 'should get new' do
+    get new_animal_url
+    assert_response :success
+  end
+
+  # test 'should create' do
+  #   assert_difference -> { Animal.count } do
+  #     post animals_url, params: {
+  #       animal: { name: @animal.name, date_of_birth: @animal.date_of_birth, animal_type: @animal.animal_type }
+  #     }
+  #   end
+
+  #   assert_redirected_to animal_url(Animal.last)
+  # end
 end
