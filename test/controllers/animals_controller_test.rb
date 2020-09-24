8cf188baf81df7f90c32b5abe35c393842899e9b
Joao Marcos <jao.markos@hotmail.com>
2020-09-24 19:35:40 -0300

    Add test for creating animal

--- test/controllers/animals_controller_test.rb
+++ test/controllers/animals_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class AnimalsControllerTest < ActionDispatch::IntegrationTest
-  # setup do
-  #   @animal = animals :dog_pet
-  # end
+  setup do
+    @animal = animals :dog_pet
+  end
 
   test 'should get index' do
     get animals_url
@@ -15,13 +15,17 @@ class AnimalsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test 'should create' do
-  #   assert_difference -> { Animal.count } do
-  #     post animals_url, params: {
-  #       animal: { name: @animal.name, date_of_birth: @animal.date_of_birth, animal_type: @animal.animal_type }
-  #     }
-  #   end
+  test 'should create animal' do
+    assert_difference 'Animal.count' do
+      post animals_url, params: {
+        animal: {
+          name: @animal.name,
+          date_of_birth: @animal.date_of_birth,
+          animal_type_id: animal_types(:dog).id
+        }
+      }
+    end
 
-  #   assert_redirected_to animal_url(Animal.last)
-  # end
+    assert_redirected_to animals_url
+  end
 end
