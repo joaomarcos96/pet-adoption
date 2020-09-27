ebe17fbaf9f9f90cf06784b5e68fe4a0bd32d4d8
Joao Marcos <jao.markos@hotmail.com>
2020-09-27 13:07:47 -0300

    Add validations to adoption model

--- app/models/adoption.rb
+++ app/models/adoption.rb
@@ -1,3 +1,6 @@
 class Adoption < ApplicationRecord
   belongs_to :animal
+  validates :adopter_name, presence: true, length: { minimum: 3 }
+  validates :adoption_date, presence: true
+  validates :animal, presence: true
 end
