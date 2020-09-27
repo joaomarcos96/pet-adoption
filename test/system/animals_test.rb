d24e45e5d12121c61672122609d932d8166f7dfe
Joao Marcos <jao.markos@hotmail.com>
2020-09-27 14:03:07 -0300

    Add system tests for animals resource

---
+++ test/system/animals_test.rb
@@ -0,0 +1,36 @@
+require 'application_system_test_case'
+
+class AnimalsTest < ApplicationSystemTestCase
+  test 'visiting the index' do
+    visit animals_url
+
+    assert_selector 'h1', text: 'Pets for adoption'
+  end
+
+  test 'creating an animal' do
+    visit animals_url
+
+    click_on 'Add new pet'
+
+    fill_in 'Name', with: 'Dogo'
+    fill_in 'Date of birth', with: '10/08/2019'
+
+    click_on 'Save pet'
+
+    assert_text 'Pet was successfully added'
+  end
+
+  test 'updating an animal' do
+    visit animals_url
+
+    click_on 'Edit', match: :first
+
+    fill_in 'Name', with: 'Pitoquinho'
+    fill_in 'Date of birth', with: '11/04/2020'
+
+    click_on 'Save pet'
+
+    assert_text 'Pet was successfully updated'
+  end
+end
+
