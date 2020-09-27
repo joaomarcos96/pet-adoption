cfc01b8c6e023e35b985ad4918af1f91f8476397
Joao Marcos <jao.markos@hotmail.com>
2020-09-27 12:52:40 -0300

    Add validation to animal model

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,6 +1,9 @@
 class Animal < ApplicationRecord
   belongs_to :animal_type
   has_one :adoption
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :animal_type, presence: true
+  validates :date_of_birth, presence: true
 
   scope :available_for_adoption, -> {
     includes(:adoption).where(adoptions: { animal_id: nil })
