d326e66d043270e741318c0dbc84ad2c6d5742a0
Joao Marcos <jao.markos@hotmail.com>
2020-09-27 14:07:05 -0300

    Add system test for adoptions resource

---
+++ test/system/adoptions_test.rb
@@ -0,0 +1,17 @@
+require 'application_system_test_case'
+
+class AdoptionsTest < ApplicationSystemTestCase
+  test 'marking an animal as adopted' do
+    visit animals_url
+
+    click_on 'Mark as adopted', match: :first
+
+    fill_in 'Adopter name', with: 'John'
+    fill_in 'Adoption date', with: '05/09/2020'
+
+    click_on 'Mark as adopted'
+
+    assert_text 'Pet was successfully marked as adopted'
+  end
+end
+
