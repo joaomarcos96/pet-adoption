560b839c801dcde966286b07db4d1ef91a619df2
Joao Marcos <jao.markos@hotmail.com>
2020-10-13 23:29:19 -0300

    Add system test for animal with photo

--- test/system/animals_test.rb
+++ test/system/animals_test.rb
@@ -20,6 +20,21 @@ class AnimalsTest < ApplicationSystemTestCase
     assert_text 'Pet was successfully added'
   end
 
+  test 'creating an animal with photo' do
+    visit animals_url
+
+    click_on 'Add new pet'
+
+    attach_file('animal[photo]', Rails.root.join('test', 'fixtures', 'files', 'test.png'))
+
+    fill_in 'Name', with: 'Dogo with pic'
+    fill_in 'Date of birth', with: '30/04/2018'
+
+    click_on 'Save pet'
+
+    assert_text 'Pet was successfully added'
+  end
+
   test 'updating an animal' do
     visit animals_url
 
